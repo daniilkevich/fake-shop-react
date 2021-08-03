80519f15f41e9c2b37997efe756798e16cbb2025
Daniil <daniilkevich@gmail.com>
2021-08-03 19:10:53 +0300

    start lesson3

--- src/index.js
+++ src/index.js
@@ -1,27 +1,18 @@
-import React, { useLayoutEffect } from 'react';
-import ReactDom from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-const App = ()
-const userAge = 10;
 
-const list = (
-
-    <ul>
-        <li>List item 1 </li>
-        <li>List item 2 </li>
-        <li>List item 3 </li>
-    </ul>
-);
-const el = (
-  <div>
-    <h1>Hello world {userAge}</h1>
-    <p>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto,
-      doloribus. Qui molestias odio sint accusantium ipsa. Vel ullam sit
-      pariatur distinctio. Pariatur mollitia assumenda optio aperiam sint,
-      dolore itaque dolorum!
-    </p>
-  </div>
-);
-
-ReactDom.render(el, document.getElementById("root"));
+const App = () => {
+  return (
+    <div>
+      <h1>Hello world </h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto,
+        doloribus. Qui molestias odio sint accusantium ipsa. Vel ullam sit
+        pariatur distinctio. Pariatur mollitia assumenda optio aperiam sint,
+        dolore itaque dolorum!
+      </p>
+    </div>
+  );
+};
+ReactDOM.render(<App />, document.getElementById("root"));

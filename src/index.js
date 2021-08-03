fba02dd33dc4e6e641e90917dec97ef87661680c
Daniil <daniilkevich@gmail.com>
2021-08-03 19:40:20 +0300

    added prettier

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
+import React from 'react'
+import ReactDOM from 'react-dom'
 
 const App = () => {
-  return (
-    <div>
-      <h1>Hello world </h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto,
-        doloribus. Qui molestias odio sint accusantium ipsa. Vel ullam sit
-        pariatur distinctio. Pariatur mollitia assumenda optio aperiam sint,
-        dolore itaque dolorum!
-      </p>
-    </div>
-  );
-};
-ReactDOM.render(<App />, document.getElementById("root"));
+    return (
+        <div>
+            <h1>Hello world </h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto,
+                doloribus. Qui molestias odio sint accusantium ipsa. Vel ullam
+                sit pariatur distinctio. Pariatur mollitia assumenda optio
+                aperiam sint, dolore itaque dolorum!
+            </p>
+        </div>
+    )
+}
+ReactDOM.render(<App />, document.getElementById('root'))

1a6be99106dd9eef8a4d7fbde632fa1429346fb4
Daniil <daniilkevich@gmail.com>
2021-08-06 16:24:03 +0300

    added menu

---
+++ src/components/Menu/Menu.js
@@ -0,0 +1,17 @@
+import React from "react"
+import Button from '@material-ui/core/Button';
+
+const Menu = () => {
+    return (
+        <>
+        <Button color="inherit" >Home</Button>
+        <Button color="inherit" >Products</Button>
+        <Button color="inherit" >Shipping</Button>
+        <Button color="inherit" >Payment</Button>
+        <Button color="inherit" >Cart</Button>
+        </>
+
+
+    )
+}
+export default Menu
--- src/conteiner/Header/Header.js
+++ src/conteiner/Header/Header.js
@@ -3,11 +3,11 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import Container from '@material-ui/core/Container';
 import { red } from '@material-ui/core/colors';
+import Menu from '../../components/Menu/Menu';
 
 
 
@@ -39,7 +39,7 @@ const Header = () => {
           <Typography variant="h6" className={classes.title}>
             Fake Shop
           </Typography>
-          <Button color="inherit" variant="outlined">Home</Button>
+          <Menu/>
         </Toolbar>
               </Container>
        

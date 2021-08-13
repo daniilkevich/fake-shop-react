6713c6966b29f7ca50f4ccdf21bf42380dfa0cc6
Daniil <daniilkevich@gmail.com>
2021-08-13 19:40:17 +0300

    added productquontity

--- src/components/Products/ProductListItem.js
+++ src/components/Products/ProductListItem.js
@@ -1,8 +1,9 @@
 
 import React from "react"
-import { Button, Card, CardActions, CardContent } from '@material-ui/core'
+import { Button, Card, CardActions, CardContent, TextField } from '@material-ui/core'
 import './ProductListItem.css'
 import PropTypes from 'prop-types'; 
+
 const ProductlistItem = ({name, description, type, capacity, price, image}) => {
     
     return <>
@@ -16,6 +17,12 @@ const ProductlistItem = ({name, description, type, capacity, price, image}) => {
                 <p>Type: {type}</p>
                 <p>Capacity: {capacity}GB</p>
                 <div className="product-price">$ {price}</div>
+                <div className="product-quantity"></div>
+                <div className="product-quantity">
+                        <Button variant="outlined">-</Button>
+                        <TextField variant="outlined" size="small" value={1} />
+                        <Button variant="outlined">+</Button>
+                    </div>
             </CardContent>
             <CardActions className="product-btn">
                     <Button variant="contained" color="primary">Add to Cart</Button>

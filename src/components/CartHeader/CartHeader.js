4e9b3a2d00127fbcffb0b9ffd99bde7e9416a042
Daniil <daniilkevich@gmail.com>
2021-08-30 19:54:38 +0300

    added productsObject

--- src/components/CartHeader/CartHeader.js
+++ src/components/CartHeader/CartHeader.js
@@ -1,12 +1,24 @@
 import React from 'react'
 import { keys } from 'lodash'
+import productsArray from '../Products/productsArray'
+
+const productsObject = productsArray.reduce(
+    (obj, product) => ({
+        ...obj,
+        [product.id]: product,
+    }),
+    {}
+)
+
+console.log(productsObject)
+console.log(productsArray)
 
 const CartHeader = ({ productsInCart }) => {
     return (
         <div>
             {keys(productsInCart).map((id) => (
                 <div key={id}>
-                    {id} : {productsInCart[id]}
+                    {productsObject[id].name} : {productsInCart[id]}
                 </div>
             ))}
         </div>
